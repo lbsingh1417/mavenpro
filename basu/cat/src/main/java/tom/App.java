7fbfcaed487e1cd93e5e504dc4833a9a53ff70a1
root <root@ip-172-31-29-109.ec2.internal>
2025-01-18 17:02:49 +0000

    basu deplyded application

--- basu/cat/src/main/java/tom/App.java
+++ basu/cat/src/main/java/tom/App.java
@@ -1,10 +1,12 @@
 package tom;
 
 /**
- * Hello world!
+ * Hello karuna this is an application 
+ * the application name is kannu world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello karuna this is an application
+			the appication name is kannu world!");
     }
 }
